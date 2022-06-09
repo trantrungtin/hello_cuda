62ddc340e077a2e23bb0e42b1521c410b2c06696
Tin Tran <tinhardware@gmail.com>
2022-06-10 00:04:31 +0700

    Calculate unique indexes

--- hello_cuda/hello_cuda.cu
+++ hello_cuda/hello_cuda.cu
@@ -9,11 +9,42 @@ __global__ void hello_cuda() {
 		threadIdx.x, threadIdx.y, threadIdx.z);
 }
 
-int main() {
+void sample1() {
 	dim3 grid(2, 2); // number of blocks
 	dim3 block(8, 2); // threads per block
 
-	hello_cuda << <grid, block>> > ();
+	hello_cuda << <grid, block >> > ();
+}
+
+__global__ void unique_idx_calc_threadIdx(int* input) {
+	int tid = threadIdx.x;
+	printf("threadIdx: %d, value: %d\n", tid, input[tid]);
+}
+
+void sample2() {
+	int array_size = 8;
+	int array_byte_size = sizeof(int) * array_size;
+	int h_data[] = { 23, 9, 4, 53, 65, 12, 1, 33 };
+
+	for (int i = 0; i < array_size; i++) {
+		printf("%d,", h_data[i]);
+	}
+	printf("\n\n");
+
+	int* d_data;
+	cudaMalloc((void**)&d_data, array_byte_size);
+	cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
+
+	dim3 block(8);
+	dim3 grid(1);
+
+	unique_idx_calc_threadIdx << <grid, block >> > (d_data);
+}
+
+int main() {
+	//sample1();
+	sample2();
+
 	cudaDeviceSynchronize();
 	cudaDeviceReset();
 	return 0;
