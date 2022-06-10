15fa267ed792e77b530c2b61982053f6348960c7
Tin Tran <tinhardware@gmail.com>
2022-06-10 12:53:59 +0700

    Measure execution time.

--- hello_cuda/hello_cuda.cu
+++ hello_cuda/hello_cuda.cu
@@ -196,7 +196,7 @@ void compare_arrays(int* a, int* b, int number_of_elements) {
 }
 
 void sum_two_arrays() {
-	const size_t number_of_elements = 1000;
+	const size_t number_of_elements = 5000000;
 	const size_t number_of_bytes = number_of_elements * sizeof(int);
 	const size_t block_size = 128;
 
@@ -206,24 +206,41 @@ void sum_two_arrays() {
 	h_c = (int*)malloc(number_of_bytes);
 	gpu_results = (int*)malloc(number_of_bytes);
 
+	clock_t htod_start, htod_end;
+	htod_start = clock();
 	int* d_a, * d_b, * d_c;
 	createCudaData(h_a, number_of_bytes, &d_a);
 	createCudaData(h_b, number_of_bytes, &d_b);
 	gpuErrchk(cudaMalloc((void**)&d_c, number_of_bytes));
+	htod_end = clock();
 
 	// launching the grid
 	dim3 block(block_size);
 	dim3 grid(number_of_elements / block_size + 1);
 
+	clock_t gpu_start, gpu_end;
+	gpu_start = clock();
 	sum_array_gpu << <grid, block >> > (d_a, d_b, d_c, number_of_elements);
 	gpuErrchk(cudaDeviceSynchronize());
+	gpu_end = clock();
 
 	// memory transfer back to host
+	clock_t dtoh_start, dtoh_end;
+	dtoh_start = clock();
 	gpuErrchk(cudaMemcpy(gpu_results, d_c, number_of_bytes, cudaMemcpyDeviceToHost));
+	dtoh_end = clock();
 
 	// array comparison
+	clock_t cpu_start, cpu_end;
+	cpu_start = clock();
 	sum_array_cpu(h_a, h_b, h_c, number_of_elements);
+	cpu_end = clock();
 	compare_arrays(h_c, gpu_results, number_of_elements);
+
+	printf("Sum array CPU execution time: %4.6f\n", (double)((double)(cpu_end - cpu_start) / CLOCKS_PER_SEC));
+	printf("Sum array GPU execution time: %4.6f\n", (double)((double)(gpu_end - gpu_start) / CLOCKS_PER_SEC));
+	printf("Transfer data from host to device: %4.6f\n", (double)((double)(htod_end - htod_start) / CLOCKS_PER_SEC));
+	printf("Transfer data from device to host: %4.6f\n", (double)((double)(dtoh_end - dtoh_start) / CLOCKS_PER_SEC));
 
 	// cleanup data
 	cudaFree(d_a);
